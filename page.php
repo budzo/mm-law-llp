7f7e33daa91f8387bb4126800518300af96a1dd3
Chris Budzban <chris@elexicon.com>
2024-03-07 11:28:14 -0600

    heading color styles

--- page.php
+++ page.php
@@ -6,15 +6,19 @@
                     <section class="page-content">
                         <div class="row justify-content-between">
                             <div class="col-lg-7">
-                                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                                    <h1><?php the_title(); ?></h1>
-                                    <?php the_content(); ?>
-                                <?php endwhile; else : ?>
-                                    <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-                                <?php endif; ?>
+                                <div class="page-content__left">
+                                    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                                        <h1><?php the_title(); ?></h1>
+                                        <?php the_content(); ?>
+                                    <?php endwhile; else : ?>
+                                        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+                                    <?php endif; ?>
+                                </div>
                             </div>
-                            <div class="col-lg-4">
-                                <?= do_shortcode('[contact-form-7 id="51ab17b" title="Contact form 1"]'); ?>
+                            <div class="col-lg-5 col-xl-4 mt-5 mt-lg-0">
+                                <div class="page-content__right">
+                                    <?= do_shortcode('[contact-form-7 id="51ab17b" title="Contact form 1"]'); ?>
+                                </div>
                             </div>
                         </div>
                     </section>
